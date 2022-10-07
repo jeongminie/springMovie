602d24379665c0c3304bcdcdfa282503af03ba15
jeongminie <opooip@nate.com>
2022-10-07 18:47:11 +0900

    221007

--- src/main/java/com/jeongmini/test/UpdateDatabaseFromExcel.java
+++ src/main/java/com/jeongmini/test/UpdateDatabaseFromExcel.java
@@ -46,7 +46,7 @@ public class UpdateDatabaseFromExcel {
 				ArrayList<String> filter = new ArrayList<String>();	// 한 행을 읽어서 저장할 변수 선언
 
 				if (row != null) {
-					int cells = 1 ;	// 셀의 수
+					int cells = 13 ;	// 셀의 수
 					cells = row.getPhysicalNumberOfCells();    // 열의 수
 					for (columnindex = 0; columnindex <= cells; columnindex++) {	// 열의 수만큼 반복
 						XSSFCell cell_filter = row.getCell(columnindex);	// 셀값을 읽는다
@@ -111,58 +111,63 @@ public class UpdateDatabaseFromExcel {
 		  
 		Connection  conn  = null;
 		PreparedStatement pstmt = null;
+		PreparedStatement pstmt2 = null;
 //		String query = "INSERT INTO theater (city, theaterCode, theaterNm, totalScreen, totalSeat, businessNm, status, adress, callNumber, homepage) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		String query = "INSERT INTO movieInfo2022 (openYear, movieCode, title, genre, nation, runningTime, age, openDate, directors, actors, story) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
+//		String query = "INSERT INTO movieInfo2022 (openYear, movieCode, title, genre, nation, runningTime, age, openDate, directors, actors, story, updatedAt) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, now())";
+		String query = "INSERT INTO runningCurrent (movieCode) values (?)";
+		String query2 = "DELETE FROM runningCurrent";
+		
 		System.out.println("총 라인 수 : "+list.size());
 		
 		try {
 			conn = getConn();	//데이터베이스 연결
 			pstmt = conn.prepareStatement(query);	//쿼리 설정
-
+			pstmt2 = conn.prepareStatement(query2);
+			
+			pstmt2.executeUpdate();
+			
 			for(int i=0; i<list.size(); i++) {      //매개변수로 받아온 ArrayList 의 길이만큼 반복한다.
 
-                //읽어온 각 셀들이 자신이 생성해준 table 제약조건과 일치하지 않을 경우 SqlException이 발생한다.
+				//읽어온 각 셀들이 자신이 생성해준 table 제약조건과 일치하지 않을 경우 SqlException이 발생한다.
                 //그러한 조건이 발생하면 continue 를 해주는 부분을 추가해주면 된다.
 				if(list.get(i).isEmpty()) continue;	//행에 값이 없을 경우에 그 행을 제외하고 진행
+				
+//				pstmt.setString(1, list.get(i).get(0));
+//				pstmt.setInt(2, Integer.parseInt(list.get(i).get(1)));
+//				pstmt.setString(3, list.get(i).get(2));
+//				pstmt.setInt(4, (int)(Double.parseDouble(list.get(i).get(3))));
+//				pstmt.setInt(5, (int)(Double.parseDouble(list.get(i).get(4))));
+//				pstmt.setString(6, list.get(i).get(5));
+//				pstmt.setString(7, list.get(i).get(6));
+//				pstmt.setString(8, list.get(i).get(7));
+//				pstmt.setString(9, list.get(i).get(8));
+//				pstmt.setString(10, list.get(i).get(9));
 				
 //				SimpleDateFormat fm = new SimpleDateFormat("yyyy-MM-dd");
 //				String test = list.get(i).get(7);
 //				Date openDate = null;
 				
-				//앞의 쿼리에서 물음표에 들어갈 항목들을 순서대로 기입
+//				앞의 쿼리에서 물음표에 들어갈 항목들을 순서대로 기입
 				pstmt.setInt(1, (int)(Double.parseDouble(list.get(i).get(0))));
-				pstmt.setInt(2, (int)(Double.parseDouble(list.get(i).get(1))));
-				pstmt.setString(3, list.get(i).get(2));
-				pstmt.setString(4, list.get(i).get(3));
-				pstmt.setString(5, list.get(i).get(4));
-				pstmt.setString(6, list.get(i).get(5));
-				pstmt.setString(7, list.get(i).get(6));
-				pstmt.setString(8, list.get(i).get(7));
+//				pstmt.setInt(2, (int)(Double.parseDouble(list.get(i).get(1))));
+//				pstmt.setString(3, list.get(i).get(2));
+//				pstmt.setString(4, list.get(i).get(3));
+//				pstmt.setString(5, list.get(i).get(4));
+//				pstmt.setString(6, list.get(i).get(5));
+//				pstmt.setString(7, list.get(i).get(6));
+//				pstmt.setString(8, list.get(i).get(7));
+//				pstmt.setString(9, list.get(i).get(8));
+//				pstmt.setString(10, list.get(i).get(9));
+//				pstmt.setString(11, list.get(i).get(10));
 //				pstmt.setString(8, fm.format(openDate));
-				pstmt.setString(9, list.get(i).get(8));
-				pstmt.setString(10, list.get(i).get(9));
-				pstmt.setString(11, list.get(i).get(10));
 				
 				//update query 실행
 				pstmt.executeUpdate();
 				
-				System.out.println(i);
-				System.out.println(list.get(i).get(0));
-				System.out.println(list.get(i).get(1));
-				System.out.println(list.get(i).get(2));
-				System.out.println(list.get(i).get(3));
-				System.out.println(list.get(i).get(4));
-				System.out.println(list.get(i).get(5));
-				System.out.println(list.get(i).get(6));
-				System.out.println(list.get(i).get(7));
-				System.out.println(list.get(i).get(8));
-				System.out.println(list.get(i).get(9));
-				System.out.println(list.get(i).get(10));
-				
 				if(i%1000==0) {
 					System.out.println(i+"번 라인 쓰는 중...");
 				}
+				
 			}
 
 			System.out.println("insert를 완료했습니다.");
